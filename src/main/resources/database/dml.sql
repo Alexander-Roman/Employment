58d7f3e06a1464226f8fd4d670e99f1f98f0fbdf
”Alexander <alexander.kitanin@gmail.com>
2021-03-01 17:55:48 +0300

    CorsFilter and test data

--- src/main/resources/database/dml.sql
+++ src/main/resources/database/dml.sql
@@ -1,4 +1,11 @@
 INSERT INTO employees (name, email, position, phone, photo, code)
-VALUES ('Александр Роман', 'alex@mail.by', 'Java Developer', '+375298642497', 'photo1', 'code1'),
-       ('Alexander Roman', 'roma@mail.by', 'JS Разработчик', '+375291111111', 'photo2', 'code2'),
-       ('Александр Roman', 'test@mail.by', 'Java Developer', '+375292222222', 'photo3', 'code3');
+VALUES ('Emma A. Main', 'EmmaMain@gmail.com', 'Graphics Designer', '+375291111111',
+        'http://localhost:8080/images/avatar/avatar8.png', 'code1'),
+       ('Paul L. Goyette', 'PaulGoyette@gmail.com', 'Graphics Designer', '+375292222222',
+        'http://localhost:8080/images/avatar/avatar6.png', 'code2'),
+       ('Jonathan Smith', 'JonathanSmith@gmail.com', 'Graphics Designer', '+375293333333',
+        'http://localhost:8080/images/avatar/avatar5.png', 'code3'),
+       ('Lily J. Ford', 'LilyFord@gmail.com', 'Graphics Designer', '+375294444444',
+        'http://localhost:8080/images/avatar/avatar3.png', 'code4'),
+       ('Александр Роман', '8642497@tut.by', 'Java Разработчик', '+375298642497',
+        'http://localhost:8080/images/avatar/alexander-roman.jpg', 'code5');
